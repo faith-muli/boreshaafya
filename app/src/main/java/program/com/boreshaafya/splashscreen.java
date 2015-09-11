ac713d3d62d1b94bd61860bd448caa67297694f7
Faith Muli <faithmuli33@gmail.com>
2015-09-11 11:15:11 +0300

    splash created

--- app/src/main/java/program/com/boreshaafya/splashscreen.java
+++ app/src/main/java/program/com/boreshaafya/splashscreen.java
@@ -1,17 +1,32 @@
 package program.com.boreshaafya;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class splashscreen extends ActionBarActivity {
+public class splashscreen extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(splashscreen.this, MainActivity.class);
+                splashscreen.this.startActivity(mainIntent);
+               splashscreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
     }
 
     @Override
